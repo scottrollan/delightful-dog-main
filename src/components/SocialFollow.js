6b89771aa88d7720daf4f669611005f180e45d93
Barry Rollan <barryrollan@barrys-mbp.lan>
2020-04-02 23:58:11 -0400

    fixed overflow x on header

--- src/components/SocialFollow.js
+++ src/components/SocialFollow.js
@@ -10,9 +10,9 @@ import {
 export default function SocialFollow() {
     return (
         <div className={styles.socialContainer}>
-            <a href="https://www.facebook.com/DelightfulDogMarietta?fref=ts" target="_blank"><FontAwesomeIcon className={styles.icon} icon={faFacebook} size="3x"></FontAwesomeIcon></a>
-            <a href="https://www.instagram.com/delightfuldog/" target="_blank"><FontAwesomeIcon className={styles.icon} icon={faInstagram} size="3x"></FontAwesomeIcon></a>
-            <a href="https://twitter.com/adoptagolden?lang=en" target="_blank"><FontAwesomeIcon className={styles.icon} icon={faTwitter} size="3x"></FontAwesomeIcon></a>
+            <a href="https://www.facebook.com/DelightfulDogMarietta?fref=ts" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className={styles.icon} icon={faFacebook} size="3x"></FontAwesomeIcon></a>
+            <a href="https://www.instagram.com/delightfuldog/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className={styles.icon} icon={faInstagram} size="3x"></FontAwesomeIcon></a>
+            <a href="https://twitter.com/adoptagolden?lang=en" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon className={styles.icon} icon={faTwitter} size="3x"></FontAwesomeIcon></a>
         </div>
     )
 }
